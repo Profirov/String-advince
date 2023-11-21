f4b01cb84fc8f47c2aa345172bb9f7e096e3b88d
user <user@DESKTOP-Q7SRQOE>
2023-11-21 19:38:44 +0200

    Add project files.

---
+++ reverse string/Program.cs
@@ -0,0 +1,11 @@
+﻿string word = Console.ReadLine();
+while (word != "end")
+{
+    string reverseWord = "";
+    for (int i = word.Length - 1; i >= 0; i -= 1)
+    {
+        reverseWord += word[i];
+    }
+    Console.WriteLine($"{word} = {reverseWord}");
+    word = Console.ReadLine();
+}
